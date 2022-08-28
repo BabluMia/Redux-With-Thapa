9109331487030367c35848c44821d8948fcce212
BabluMia <bablumiah2000@gmail.com>
2022-08-28 23:54:09 +0600

    redux working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import UseReducer from './UseReducer/UseReducer';
 function App() {
   return (
     <div className="App">
-      <UseReducer/>
+      {/* <UseReducer/> */}
+      <h3 style={{ color: "skyblue", fontSize: "50px" }}>Welcome To Redux</h3>
+      <h3>0</h3>
+      <div>
+        <button >
+          Increment
+        </button>
+        <button >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
